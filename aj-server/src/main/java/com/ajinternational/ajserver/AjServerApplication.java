912bd879bd97ad501196ca70a628af7096b3a39d
okanacer332 <acer.okanumut@hotmail.com>
2025-09-15 12:40:46 +0300

    update

---
+++ aj-server/src/main/java/com/ajinternational/ajserver/AjServerApplication.java
@@ -0,0 +1,13 @@
+package com.ajinternational.ajserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjServerApplication.class, args);
+	}
+
+}
---
+++ aj-server/src/test/java/com/ajinternational/ajserver/AjServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajinternational.ajserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
