939868df32ba3df3bd0921b01f01e595f5b707bb
okanacer332 <acer.okanumut@hotmail.com>
2025-09-30 13:33:49 +0300

    updrrr

--- aj-client/src/app/page.tsx
+++ aj-client/src/app/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-// Bu satır, Next.js'e bu sayfanın statik olarak oluşturulmaması gerektiğini söyler.
-export const dynamic = "force-dynamic";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-    const router = useRouter();
-
-    useEffect(() => {
-        router.push("/dashboard/default");
-    }, [router]);
-
-    return null; // Bu sayfa hiçbir şey render etmeyecek, sadece yönlendirecek.
+  redirect('/dashboard/default');
 }
